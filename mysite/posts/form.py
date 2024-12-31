a3db3faecacff51875e78d0dc56ad3f44439227e
graceyong1020 <graceyong1020@gmail.com>
2024-12-31 14:36:33 +0900

    cru - complete

---
+++ mysite/posts/form.py
@@ -0,0 +1,54 @@
+from django import forms
+from .models import Posts
+
+#  from
+class PostCreateForm(forms.ModelForm):
+   title = forms.CharField(required=False)
+   content = forms.CharField(required=False)
+   username = forms.CharField(required=False)
+   password = forms.CharField(required=False)
+
+   class Meta:
+     model = Posts
+     fields = ['title', 'content', 'username', 'password']
+
+   # validation check for title
+   def clean_title(self):
+     title = self.cleaned_data['title']
+     if not title:
+         raise forms.ValidationError('Title is required')
+     if len(title) < 2:
+         raise forms.ValidationError('Title must be at least 2 characters')
+     if len(title) > 100:
+         raise forms.ValidationError('Title must be less than 100 characters')
+     return title
+   
+   # validation check for content
+   def clean_content(self):
+     content = self.cleaned_data['content']
+     if not content:
+         raise forms.ValidationError('Content is required')
+     return content
+   
+   # validation check for username
+   def clean_username(self):
+     username = self.cleaned_data['username']
+     if not username:
+         raise forms.ValidationError('Username is required')
+     if len(username) < 2:
+         raise forms.ValidationError('Username must be at least 2 characters')
+     if len(username) > 100:
+         raise forms.ValidationError('Username must be less than 100 characters')
+     return username
+   
+   # validation check for password
+   def clean_password(self):
+     password = self.cleaned_data['password']
+     if not password:
+         raise forms.ValidationError('Password is required')
+     if len(password) < 4:
+         raise forms.ValidationError('Password must be at least 4 characters')
+     if len(password) > 20:
+         raise forms.ValidationError('Password must be less than 20 characters')
+     return password
+   
--- mysite/posts/views.py
+++ mysite/posts/views.py
@@ -1,3 +1,70 @@
-from django.shortcuts import render
+from django.http import HttpResponse
+
+from django.shortcuts import render, redirect, get_object_or_404
+from .models import Posts
+
+from django.contrib import messages
+from .form import PostCreateForm
+from .form2 import PostUpdateForm
 
 # Create your views here.
+
+# register
+""" def create_post(request):
+    return HttpResponse("Create post") """
+def create_post(request):
+    form = PostCreateForm()
+
+    if request.method == 'POST':
+        form = PostCreateForm(request.POST)
+        if form.is_valid():
+           post = form.save(commit=False)
+           post.save()
+           messages.success(request, 'Post created successfully')
+           return redirect('posts:read', post_id=post.id)
+        else:
+            messages.error(request, 'Post creation failed')
+    return render(request, 'posts/create.html', {'form': form})
+
+# Read
+""" def get_post(request, post_id):
+    return HttpResponse('Get post')
+ """
+def get_post(request, post_id):
+    post = get_object_or_404(Posts, id=post_id)
+    return render(request, 'posts/read.html', {'post': post})
+
+# Update
+""" def update_post(request, post_id):
+    return HttpResponse('Update post') """
+def update_post(request, post_id):
+    post = get_object_or_404(Posts, id=post_id)
+    form = PostUpdateForm(instance=post)
+
+    if request.method == 'POST':
+        form = PostUpdateForm(request.POST)
+
+        if form.is_valid():
+            if form.cleaned_data['password'] == post.password:
+                post = form.save(commit=False)
+                post.save()
+                messages.success(request, 'Post updated successfully')
+                return redirect('posts:read', post_id=post.id)
+            else:
+                messages.error(request, 'Password is incorrect')
+        else:
+            messages.error(request, 'Post update failed')
+    return render(request, 'posts/update.html', {'form': form})
+
+
+# Delete
+def delete_post(request, post_id):
+    return HttpResponse('Delete post')
+
+# List
+""" def get_posts(request):
+    return HttpResponse('Get List of posts') """
+
+def get_posts(request):
+    posts = Posts.objects.all().order_by('-created_at')
+    return render(request, 'posts/list.html', {'posts': posts})
